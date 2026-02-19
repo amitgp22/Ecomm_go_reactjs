2273c87506fc0155fca719ed38939f297c96dc98
amitgp22 <amitgp9009@gmail.com>
2026-02-20 01:46:36 +0530

    go vicky file add

--- controllers/Vickycotrooler.go
+++ controllers/Vickycotrooler.go
@@ -8,7 +8,7 @@ import (
 )
 
 func GetProducts1(w http.ResponseWriter, r *http.Request) {
-	var products []models.Product
+	var products []models.Products
 	config.DB.Find(&products)
 	json.NewEncoder(w).Encode(products)
 }
