65f26ba61e08a0b5711d6e757891d69b9e36a03f
amitgp22 <amitgp9009@gmail.com>
2026-02-20 01:24:43 +0530

    go vicky file add

---
+++ controllers/Vickycotrooler.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"ecommerce-api/config"
+	"ecommerce-api/models"
+	"encoding/json"
+	"net/http"
+
+)
+
+func GetProducts1(w http.ResponseWriter, r *http.Request) {
+	var products []models.Product
+	config.DB.Find(&products)
+	json.NewEncoder(w).Encode(products)
+}
