eae7d3464ecbcc53d75d19a1e4727fed0f848e5b
amitgp22 <amitgp9009@gmail.com>
2026-02-20 01:57:08 +0530

    go vicky file mergerd124

--- controllers/Vickycotrooler.go
+++ controllers/Vickycotrooler.go
@@ -8,7 +8,7 @@ import (
 )
 
 func GetProducts1(w http.ResponseWriter, r *http.Request) {
-	var products []models.Products223
+	var products []models.Products223w
 	config.DB.Find(&products)
 	json.NewEncoder(w).Encode(products)
 }
